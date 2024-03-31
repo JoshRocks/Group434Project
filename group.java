097f920361cdac9888e951cf3d88d38634e724ff
JRock <50375578+JoshRocks@users.noreply.github.com>
2024-03-31 02:17:47 -0400

    updatedPersonalBranch

---
+++ group.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public String getJoshName()
+{
+	return "Joshua Rockamore";
+}
+
+public String getGroupMembers() {
+	String[] names = new String[4];
+	names[0] = getJoshName();
+
+
+	return names;
+}
