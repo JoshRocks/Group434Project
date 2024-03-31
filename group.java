f88af5b3413241e3fe9f628619821d0e95734da0
JRock <50375578+JoshRocks@users.noreply.github.com>
2024-03-31 02:14:01 -0400

    No edits

--- group.java
+++ group.java
@@ -5,8 +5,7 @@ public String getJoshName()
 	return "Joshua Rockamore";
 }
 
-public String getGroupMembers()
-{
+public String getGroupMembers() {
 	String[] names = new String[4];
 	names[0] = getJoshName();
 
