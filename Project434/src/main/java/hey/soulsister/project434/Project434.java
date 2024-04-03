a03333635bf213e947422d43b18e907619de712a
JRock <50375578+JoshRocks@users.noreply.github.com>
2024-04-03 13:14:59 -0400

    TurnedGroupFileIntoProject

---
+++ Project434/src/main/java/hey/soulsister/project434/Project434.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hey.soulsister.project434;
+
+import hey.soulsister.group.Group;
+
+/**
+ *
+ * @author rocka
+ */
+public class Project434 {
+
+    public static void main(String[] args) 
+    {
+        
+        Group MathMaker = new Group();
+
+        System.out.println(MathMaker.getGroupMembers());
+    }
+}
